fa05803e1dfa35e648586aac50bc9c8137414335
stefanbe <mozilo@stefanbe.de>
2010-03-19 15:44:45 +0000

    Toltips ' wird escapt

--- admin/string.php
+++ admin/string.php
@@ -16,15 +16,15 @@ function createTooltipWZ($title, $body,$parameter = NULL)
     if($title != "" and $body == "") {
         if(!isset($BASIC_LANGUAGE->properties[$title]))
             return NULL;
-        return ' onmouseover="Tip(\'<b>'.trim(getLanguageValue($title)).'</b>\''.$parameter.')" onmouseout="UnTip()"';
+        return ' onmouseover="Tip(\'<b>'.str_replace("'","\'",trim(getLanguageValue($title))).'</b>\''.$parameter.')" onmouseout="UnTip()"';
     } elseif($title == "" and $body != "") {
         if(!isset($BASIC_LANGUAGE->properties[$body]))
             return NULL;
-        return ' onmouseover="Tip(\''.trim(getLanguageValue($body)).'\''.$parameter.')" onmouseout="UnTip()"';
+        return ' onmouseover="Tip(\''.str_replace("'","\'",trim(getLanguageValue($body))).'\''.$parameter.')" onmouseout="UnTip()"';
     } else {
         if(!isset($BASIC_LANGUAGE->properties[$title]) and !isset($BASIC_LANGUAGE->properties[$body]))
             return NULL;
-        return ' onmouseover="Tip(\'<b>'.trim(getLanguageValue($title)).'</b><br />'.trim(getLanguageValue($body)).'\''.$parameter.')" onmouseout="UnTip()"';
+        return ' onmouseover="Tip(\'<b>'.str_replace("'","\'",trim(getLanguageValue($title))).'</b><br />'.str_replace("'","\'",trim(getLanguageValue($body))).'\''.$parameter.')" onmouseout="UnTip()"';
     }
 }
     
