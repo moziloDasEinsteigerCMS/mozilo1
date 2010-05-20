d3c6b01a53cc60da6a955639e446032433b6b127
azett <arvid.zimmermann@mozilo.de>
2010-05-20 20:54:15 +0000

    Vorbereitung für den Beta-Test: CMS-Version von "1.12" auf "1.12.beta1" geändert.

--- cms/DefaultConf.php
+++ cms/DefaultConf.php
@@ -168,7 +168,7 @@ function makeDefaultConf($conf_datei) {
 
     $downloads = array('_downloadcounterstarttime' => time());
 
-    $version = array('cmsversion' => '1.12',
+    $version = array('cmsversion' => '1.12.beta1',
                         'cmsname' => 'Amalia',
                         'revision' => '629');
 
