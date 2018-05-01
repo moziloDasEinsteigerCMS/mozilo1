da9e4ed831cb6d4c2622d407503dc30d78ea46ce
azett <azett@users.noreply.github.com>
2018-05-01 12:11:03 +0200

    Bugfix in der Suche und für PHP7
    Suche: Highlighting für Suchbegriffe gefixt; getDirAsArray() berücksichtigt auch Symlinks; obsoleten e-Modifier für preg_replace entfernt

---
+++ DefaultConf.php
@@ -0,0 +1,297 @@
+<?php
+
+$CHARSET                = 'UTF-8';
+
+$CONTENT_DIR_NAME       = "kategorien";
+$CONTENT_FILES_DIR_NAME = "dateien";
+$PLUGIN_DIR_NAME        = "plugins";
+$GALLERIES_DIR_NAME     = "galerien";
+$PREVIEW_DIR_NAME       = "vorschau";
+// Dateiendungen fuer Inhaltsseiten
+# Achtung die endungen muessen alle gleich lang sein
+$EXT_PAGE       = ".txt";
+$EXT_HIDDEN     = ".hid";
+$EXT_DRAFT      = ".tmp";
+$EXT_LINK       = ".lnk";
+
+define("CHARSET",$CHARSET);
+define("CONTENT_DIR_NAME",$CONTENT_DIR_NAME);
+define("CONTENT_FILES_DIR_NAME",$CONTENT_FILES_DIR_NAME);
+define("PLUGIN_DIR_NAME",$PLUGIN_DIR_NAME);
+define("GALLERIES_DIR_NAME",$GALLERIES_DIR_NAME);
+define("PREVIEW_DIR_NAME",$PREVIEW_DIR_NAME);
+define("EXT_PAGE",$EXT_PAGE);
+define("EXT_HIDDEN",$EXT_HIDDEN);
+define("EXT_DRAFT",$EXT_DRAFT);
+define("EXT_LINK",$EXT_LINK);
+
+function cleanREQUEST($post_return) {
+    foreach($post_return as $key => $value) {
+        if(is_array($post_return[$key])) {
+            $post_return[$key] = cleanREQUEST($post_return[$key]);
+        } else {
+            // Nullbytes abfangen!
+            if (strpos("tmp".$value, "\x00") > 0 or strpos("tmp".$key, "\x00") > 0) {
+                die();
+            }
+            # auf manchen Systemen mus ein stripslashes() gemacht werden
+            if(strpos("tmp".$value,'\\') > 0
+                and  addslashes(stripslashes($value)) == $value) {
+                $value = stripslashes($value);
+            }
+            # auf manchen Systemen mus ein stripslashes() gemacht werden
+            if(strpos("tmp".$key,'\\') > 0
+                and  addslashes(stripslashes($key)) == $key) {
+                $value = stripslashes($key);
+            }
+            $post_return[$key] = $value;
+        }
+    }
+    return $post_return;
+}
+
+# Alle Platzhalter
+function makePlatzhalter($all = false) {
+    # Alle Platzhalter für die Selctbox im Editor als array
+    $platzhalter = array(
+                        '{BASE_URL}',
+                        '{CATEGORY_NAME}',
+                        '{CATEGORY}',
+                        '{CATEGORY_URL}',
+                        '{PAGE_NAME}',
+                        '{PAGE_FILE}',
+                        '{PAGE_URL}',
+                        '{PAGE}',
+                        '{SEARCH}',
+                        '{SITEMAPLINK}',
+                        '{CMSINFO}',
+                        '{TABLEOFCONTENTS}'
+    );
+    # Die Rstlichen Platzhalter
+    $platzhalter_rest = array(
+                        '{CSS_FILE}',
+                        '{CHARSET}',
+                        '{FAVICON_FILE}',
+                        '{LAYOUT_DIR}',
+                        '{WEBSITE_TITLE}',
+                        '{WEBSITE_KEYWORDS}',
+                        '{WEBSITE_DESCRIPTION}',
+                        '{WEBSITE_NAME}',
+                        '{MAINMENU}',
+                        '{DETAILMENU}',
+                        '{MEMORYUSAGE}',
+                        '{EXECUTETIME}'
+    );
+
+    if($all) {
+        $platzhalter = array_merge($platzhalter,$platzhalter_rest);
+    }
+    return $platzhalter;
+}
+
+# $conf_datei = voller pfad und conf Dateiname oder nur Array Name
+function makeDefaultConf($conf_datei) {
+    $basic = array(
+                    'text' => array(
+                        'adminmail' => '',
+                        'language' => 'deDE',
+                        'noupload' => 'php,php3,php4,php5'),
+                    'digit' => array(
+                        'backupmsgintervall' => '30',
+                        'chmodnewfilesatts' => '',
+                        'lastbackup' => time(),
+                        'maximageheight' => '',
+                        'maximagewidth' => '',
+                        'maxnumberofuploadfiles' => '5',
+                        'textareaheight' => '270'),
+                    'checkbox' => array(
+                        'overwriteuploadfiles' => 'false',
+                        'sendadminmail' => 'false',
+                        'showTooltips' => 'true',
+                        'usebigactionicons' => 'false',
+                        'showexpert' => 'false'),
+                    # das sind die Expert Parameter von basic
+                    'expert' => array(
+                        'noupload',
+                        'backupmsgintervall',
+                        'lastbackup',
+                        'maxnumberofuploadfiles',
+                        'showTooltips',
+                        'textareaheight',
+                        'usebigactionicons',
+                        'overwriteuploadfiles')
+                    );
+
+    $main = array(
+                    'text' => array(
+                        'shortenlinks' => '0',
+                        'titlebarseparator' => '%20%3A%3A%20',
+                        'usesubmenu' => '1',
+                        'websitedescription' => '',
+                        'websitekeywords' => '',
+                        'websitetitle' => 'moziloCMS%20-%20Das%20CMS%20f%C3%BCr%20Einsteiger'),
+                    'select' => array(
+                        'cmslanguage' => 'deDE',
+                        'cmslayout' => 'moziloCMS',
+                        'defaultcat' => '10_Willkommen',
+                        'titlebarformat' => '%7BWEBSITE%7D'),
+                    'checkbox' => array(
+                        'hidecatnamedpages' => 'false',
+                        'modrewrite' => 'false',
+                        'replaceemoticons' => 'true',
+                        'showhiddenpagesasdefaultpage' => ' false',
+                        'showhiddenpagesincmsvariables' => ' false',
+                        'showhiddenpagesinlastchanged' => 'false',
+                        'showhiddenpagesinsearch' => 'false',
+                        'showhiddenpagesinsitemap' => 'false',
+                        'showsyntaxtooltips' => 'true',
+                        'targetblank_download' => 'true',
+                        'targetblank_link' => 'true',
+                        'usecmssyntax' => 'true',
+                        'usecmseditarea' => 'true'),
+                    # das sind die Expert Parameter von main
+                    'expert' => array(
+                        'hidecatnamedpages',
+                        'modrewrite',
+                        'showhiddenpagesasdefaultpage',
+                        'showhiddenpagesincmsvariables',
+                        'showhiddenpagesinlastchanged',
+                        'showhiddenpagesinsearch',
+                        'showhiddenpagesinsitemap',
+                        'targetblank_download',
+                        'targetblank_link',
+                        'showsyntaxtooltips',
+                        'replaceemoticons',
+                        'shortenlinks',
+                        'usecmssyntax',
+                        'usesubmenu',
+                        'usecmseditarea')
+                    );
+
+    $syntax = array('wikipedia' => '[link={DESCRIPTION}|http://de.wikipedia.org/wiki/{VALUE}]');
+/*
+    $formular = array('formularmail' => '',
+                        'contactformusespamprotection' => 'true',
+                        'contactformwaittime' => '15',
+                        'mail' => ',true,true',
+                        'message' => ',true,true',
+                        'name' => ',true,true',
+                        'website' => ',true,false');
+*/
+    $logindata = array('falselogincount' => '0',
+                        'falselogincounttemp' => '0',
+                        'initialpw' => 'true',
+                        'initialsetup' => 'true',
+                        'loginlockstarttime' => '',
+                        'name' => 'admin',
+                        'pw' => '19ad89bc3e3c9d7ef68b89523eff1987');
+
+    $downloads = array('_downloadcounterstarttime' => time());
+
+    $version = array('cmsversion' => '1.12.php7',
+                        'cmsname' => 'Amalia',
+                        'revision' => '958');
+
+    $gallery = array('digit' => array(
+                        'maxheight' => '',
+                        'maxwidth' => '',
+                        'maxthumbheight' => '100',
+                        'maxthumbwidth' => '100'),
+##                  'checkbox' => array(
+##                      'createthumbs' => 'true'),
+                    'expert' => array(
+##                      'createthumbs',
+                        'maxthumbheight',
+                        'maxthumbwidth')
+                    );
+
+    $plugin = array('active' => 'true');
+
+    $passwords = array('# Kategorie/Inhaltsseite' => 'password');
+
+    # ist eine *.conf datei angegeben wird das jeweilige array ohne expert und nur der inhalt der subarrays zurückgegeben
+    if(strpos($conf_datei,".conf") > 0) {
+        $name = substr(basename($conf_datei),0,-(strlen(".conf")));
+        # beim erzeugen duerfen sub arrays nicht mit rein
+        foreach($$name as $key => $value) {
+            if($key == "expert") continue;
+            if(is_array($value)) {
+                foreach($value as $key => $value) {
+                    $return_array[$key] = $value;
+                }
+            } else {
+                $return_array = $$name;
+                break;
+            }
+        }
+        return $return_array;
+    # ist es keine *.conf einfach das ganze array zurück
+    } else {
+        return $$conf_datei;
+    }
+}
+
+// ------------------------------------------------------------------------------
+// Handelt es sich um ein valides Verzeichnis / eine valide Datei?
+// ------------------------------------------------------------------------------
+function isValidDirOrFile($file) {
+    # Alles, was einen Punkt vor der Datei hat
+    if(strpos($file,".") === 0) {
+        return false;
+    }
+    # alle PHP-Dateien
+    if(substr($file,-4) == ".php") {
+        return false;
+    }
+    # ...und der Rest
+    if(in_array($file, array(
+            "Thumbs.db", // Windows-spezifisch
+            "__MACOSX", // Mac-spezifisch
+            "settings" // Eclipse
+            ))) {
+        return false;
+    }
+    return true;
+}
+
+# $filetype = "dir" nur ordner
+# $filetype = "file" nur dateien
+# $filetype = array(".txt",".hid",...) nur die mit dieser ext
+#               Achtung Punkt nicht vergessen Gross/Kleinschreibung ist egal
+# $filetype = false alle dateien
+# $sort_type = "sort" (Default) oder "natcasesort" oder "none"
+function getDirAsArray($dir,$filetype = false,$sort_type = "sort") {
+    $dateien = array();
+    if(is_dir($dir) and false !== ($currentdir = opendir($dir))) {
+        while(false !== ($file = readdir($currentdir))) {
+            # keine gültige datei gleich zur nächsten datei
+            if(!isValidDirOrFile($file))
+                continue;
+            # nur mit ext
+            if(is_array($filetype)) {
+                # alle ext im array in kleinschreibung wandeln
+                $filetype = array_map('strtolower', $filetype);
+                $ext = strtolower(substr($file,strrpos($file,".")));
+                if(in_array($ext,$filetype)) {
+                    $dateien[] = $file;
+                }
+			// AZI 2017-09-17: Auch Symlinks müssen beachtet werden
+            //} elseif(filetype($dir."/".$file) == $filetype) {
+			} elseif(filetype($dir."/".$file) == $filetype || filetype($dir."/".$file) == 'link') {
+			// /AZI
+                $dateien[] = $file;
+            # alle
+            } elseif(!$filetype) {
+                $dateien[] = $file;
+            }
+        }
+        closedir($currentdir);
+        if($sort_type == "sort")
+            sort($dateien);
+        elseif($sort_type == "natcasesort")
+            natcasesort($dateien);
+    }
+    return $dateien;
+}
+
+?>
---
+++ Search.php
@@ -0,0 +1,166 @@
+<?php
+
+function getSearchString($searchstring,$array = true) {
+    if(trim($searchstring) != "") {
+        $string = preg_replace("/[\"|\'](.*)[\"|\']/Umsi", "str_replace(\" \",\"%20\",'\\1')", $searchstring);
+        $return_array = array();
+        $matches = explode(" ",$string);
+        foreach($matches as $string) {
+            $string = str_replace("%20"," ",trim($string));
+            if(!empty($string))
+                $return_array[] = $string;
+        }
+        if($array)
+            return $return_array;
+        else
+            return implode(",", $return_array);
+    } else
+        return false;
+}
+
+
+function searchInPages() {
+    global $CMS_CONF;
+    global $QUERY_REQUEST;
+    global $specialchars;
+    global $language;
+    global $CatPage;
+
+    if(!$highlightparameter = getSearchString($QUERY_REQUEST,false))
+        return false;
+
+    $include_pages = array(EXT_PAGE);
+    if($CMS_CONF->get("showhiddenpagesinsearch") == "true")
+        $include_pages = array(EXT_PAGE,EXT_HIDDEN);
+
+    // Kategorien-Verzeichnis einlesen
+    $categoriesarray = $CatPage->get_CatArray(false, false, $include_pages);
+    $matchingpages = array();
+    // Alle Kategorien durchsuchen
+    foreach ($categoriesarray as $currentcategory) {
+        $contentarray = $CatPage->get_PageArray($currentcategory,$include_pages,true);
+        // Alle Inhaltsseiten durchsuchen
+        foreach ($contentarray as $currentcontent) {
+            // Treffer in der aktuellen Seite?
+            if (searchPage($currentcategory,$currentcontent)) {
+                $matchingpages[$currentcategory][$currentcontent] = "true";
+            }
+        }
+    }
+    $searchresults = "";
+    $matchesoverall = 0;
+    foreach ($matchingpages as $cat => $tmp) {
+        $categoryname = $CatPage->get_HrefText($cat,false);
+        $searchresults .= "<h2>$categoryname</h2><ul>";
+        if(!isset($matchingpages[$cat])) continue;
+        foreach ($matchingpages[$cat] as $page => $tmp) {
+            if($tmp != "true") continue;
+            $matchesoverall++;
+            $pagename = $CatPage->get_HrefText($cat,$page);
+            $searchresults .= "<li>".
+                $CatPage->create_LinkTag($CatPage->get_Href($cat,$page,"highlight=".$specialchars->replaceSpecialChars($highlightparameter,false)),
+                highlightSearch($pagename,$highlightparameter),
+                false,
+                $titel = $language->getLanguageValue2("tooltip_link_page_2", $pagename, $categoryname)
+                ).
+                "</li>";
+        }
+        $searchresults .= "</ul>";
+    }
+    // Keine Inhalte gefunden?
+    if ($matchesoverall == 0)
+        $searchresults .= $language->getLanguageValue0("message_nodatafound_0", trim($QUERY_REQUEST));
+    // Rueckgabe des Menues
+    return $searchresults;
+}
+
+function searchPage($cat,$page) {
+    global $syntax;
+    global $QUERY_REQUEST;
+    global $specialchars;
+    global $CatPage;
+
+    if(!$queryarray = getSearchString($QUERY_REQUEST))
+        return false;
+
+    // Dateiinhalt auslesen, wenn vorhanden...
+    if (false !== ($pagecontent = $CatPage->get_PageContent($cat,$page))) {
+        if(empty($pagecontent))
+            return false;
+        $pagecontent = $syntax->convertContent($pagecontent, $cat, true);
+
+        # alle Komentare raus
+        $pagecontent = preg_replace("/\<!--(.*)-->/Umsi"," ", $pagecontent);
+        # alle script, select, object, embed sachen raus
+        $pagecontent = preg_replace("/\<script(.*)\<\/script>/Umsi", "", $pagecontent);
+        $pagecontent = preg_replace("/\<select(.*)\<\/select>/Umsi", "", $pagecontent);
+        $pagecontent = preg_replace("/\<object(.*)\<\/object>/Umsi", "", $pagecontent);
+        $pagecontent = preg_replace("/\<embed(.*)\<\/embed>/Umsi", "", $pagecontent);
+        # alle tags raus
+        $pagecontent = strip_tags($pagecontent);
+        # Achtung strtolower macht keine Umlaute deshalb bleiben Sonderzeichen in html
+        $pagecontent = strtolower($pagecontent);
+        # nach alle Suchbegrieffe suchen
+        foreach($queryarray as $query) {
+            if ($query == "")
+                continue;
+#            $query = $specialchars->rebuildSpecialChars($query, false, true);
+            // Wenn...
+            if (
+                // ...der aktuelle Suchbegriff im Seitennamen...
+                (substr_count(strtolower($CatPage->get_HrefText($cat,$page)), strtolower($query)) > 0)
+                // ...oder im eigentlichen Seiteninhalt vorkommt
+                or (substr_count($pagecontent, strtolower($query)) > 0)
+                ) {
+                // gefunden
+                return true;
+            }
+        }
+    } else
+        return false;
+}
+
+// ------------------------------------------------------------------------------
+// Phrasen in Inhalt hervorheben
+// ------------------------------------------------------------------------------
+function highlightSearch($content, $phrasestring) {
+	
+	// AZI: Suche war kaputt :(
+	$result = $content;
+    // Zu highlightende Begriffe kommen kommasepariert ("begriff1,begriff2")-> in Array wandeln
+    $phrasearray = explode(",", $phrasestring);
+    // jeden Begriff highlighten
+    foreach($phrasearray as $phrase) {
+		$result = preg_replace('/(<.+?>[^<>]*?)('.preg_quote($phrase).')([^<>]*?<.+?>)/iu', '\1<span class="highlight">\2</span>\3', $result); 
+	}
+	return $result;
+	// /AZI
+	
+	//return preg_replace("/($phrasestring)/iu", '<span class="highlight">\1</span>', $content); 
+    global $specialchars;
+    global $syntax;
+    # in $syntax den content setzen
+    $syntax->content = $content;
+    # alle script style sachen mit dumy ersetzen
+    $syntax->find_script_style();
+    // Zu highlightende Begriffe kommen kommasepariert ("begriff1,begriff2")-> in Array wandeln
+    $phrasearray = explode(",", $phrasestring);
+    // jeden Begriff highlighten
+    foreach($phrasearray as $phrase) {
+#        $phrase = $specialchars->rebuildSpecialChars($phrase, false, true);
+        // Regex-Zeichen im zu highlightenden Text escapen (.\+*?[^]$(){}=!<>|:)
+        $phrase = preg_quote($phrase);
+        // Slashes im zu highlightenden Text escapen
+        $phrase = preg_replace("/\//", "\\\\/", $phrase);
+        # die such worte hervorheben
+#!!!!!! wir brauchen eine regex die nicht in script style tags text hervorhebt
+# dann kann auch das find_script_style() wieder raus
+        $syntax->content = preg_replace("/((<[^>]*)|$phrase)/iu", '"\2"=="\1"? "\1":"<span class=\"highlight\">\1</span>"', $syntax->content); 
+    }
+    # alle script style sachen wieder einsetzen
+    $syntax->find_script_style(false);
+    # inhalt zurück
+    return $syntax->content;
+}
+
+?>
